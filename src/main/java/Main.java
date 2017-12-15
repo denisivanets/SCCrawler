866b7cf4ce634ff999b49ad06f5f8ac279a4fb52
Denis Ivanets <denisivanets@gmail.com>
2017-12-15 22:59:57 +0300

    yep

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import com.google.common.reflect.ClassPath;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private Map<String, List<String>> counter = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.inspectCode();
+        main.printResult();
+    }
+
+    private void inspectCode() throws Exception {
+        ClassPath.from(ClassLoader.getSystemClassLoader())
+                .getAllClasses()
+                .stream()
+                .forEach(this::processClass);
+    }
+
+    private boolean notJustObject(ClassPath.ClassInfo clazz) {
+        return !clazz.getName().endsWith("Object");
+    }
+
+    private void processClass(final ClassPath.ClassInfo clazz) {
+        String parent = clazz.load().getGenericSuperclass().getTypeName();
+        if (!counter.containsKey(parent)) {
+            List<String> childClasses = new ArrayList<>();
+            childClasses.add(clazz.getName());
+            counter.put(parent, childClasses);
+        } else {
+            counter.get(parent).add(clazz.getName());
+        }
+    }
+
+    private void printResult() {
+        for (Map.Entry<String, List<String>> entry : counter.entrySet()) {
+            System.out.println(entry.getKey() + ": " + printChild(entry.getValue()));
+        }
+    }
+
+    private String printChild(final List<String> classes) {
+        return String.join(", ", classes);
+    }
+
+}
+
